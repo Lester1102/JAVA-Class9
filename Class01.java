aa507f2e2c04fcf2e63f320737d6d5273a8e91f3
Lester1102 <91860948+Lester1102@users.noreply.github.com>
2021-11-14 17:13:15 +0800

    Add files via upload

---
+++ Class01.java
@@ -0,0 +1,31 @@
+class CRectangle 
+ { 
+  int width; 
+  int height; 
+ public CRectangle(int w,int h) 
+ { 
+ width=w; 
+ height=h; 
+ } 
+ public CRectangle()  
+ { 
+ width=10; 
+ height=8; 
+ } 
+  public void show() 
+  { 
+  System.out.println("width="+width); 
+  System.out.println("height="+height); 
+  } 
+  } 
+  
+  public class Class01 
+  { 
+  public static void main(String args[]) 
+  { 
+  CRectangle rec1=new CRectangle(10,20); 
+  rec1.show(); 
+  CRectangle rec2=new CRectangle(); 
+  rec2.show(); 
+  } 
+  } 
